67d29d4259778e77ce6fb5367aaa1459195b3fba
Loris Fiard <lorisfiard1@gmail.com>
2024-10-25 08:18:56 +0200

    shape drawer

--- GlacoxEngineSFML/ShapeDrawer.cpp
+++ GlacoxEngineSFML/ShapeDrawer.cpp
@@ -1,1 +1,65 @@
 #include "ShapeDrawer.h"
+
+ShapeDrawer::ShapeDrawer()
+{
+}
+
+sf::RectangleShape ShapeDrawer::CreateSquare(float size, sf::Color color, float posx, float posy, float OutlineThickness, sf::Color OutlineColor)
+{
+	sf::RectangleShape square(sf::Vector2f(size, size));
+	square.setFillColor(color);
+	square.setPosition(sf::Vector2f(posx, posy));
+
+	square.setOutlineThickness(OutlineThickness);
+	square.setOutlineColor(OutlineColor);
+
+	return square;
+}
+
+sf::RectangleShape ShapeDrawer::CreateRectangle(float sizex, float sizey, sf::Color color, float posx, float posy, float OutlineThickness, sf::Color OutlineColor)
+{
+	sf::RectangleShape rectangle(sf::Vector2f(sizex, sizey));
+	rectangle.setFillColor(color);
+	rectangle.setPosition(sf::Vector2f(posx, posy));
+
+	rectangle.setOutlineThickness(OutlineThickness);
+	rectangle.setOutlineColor(OutlineColor);
+
+	return rectangle;
+}
+
+sf::CircleShape ShapeDrawer::CreateCircle(float radius, sf::Color color, float posx, float posy, float OutlineThickness, sf::Color OutlineColor)
+{
+	sf::CircleShape circle(radius);
+	circle.setFillColor(color);
+	circle.setPosition(sf::Vector2f(posx, posy));
+
+	circle.setOutlineThickness(OutlineThickness);
+	circle.setOutlineColor(OutlineColor);
+
+	return circle;
+}
+
+sf::CircleShape ShapeDrawer::CreateTriangle(float radius, sf::Color color, float posx, float posy, float OutlineThickness, sf::Color OutlineColor)
+{
+	sf::CircleShape triangle(radius, 3);
+	triangle.setFillColor(color);
+	triangle.setPosition(sf::Vector2f(posx, posy));
+
+	triangle.setOutlineThickness(OutlineThickness);
+	triangle.setOutlineColor(OutlineColor);
+
+	return triangle;
+}
+
+sf::CircleShape ShapeDrawer::CreatePolygon(float radius, int sideNumber, sf::Color color, float posx, float posy, float OutlineThickness, sf::Color OutlineColor)
+{
+	sf::CircleShape polygon(radius, sideNumber);
+	polygon.setFillColor(color);
+	polygon.setPosition(sf::Vector2f(posx, posy));
+
+	polygon.setOutlineThickness(OutlineThickness);
+	polygon.setOutlineColor(OutlineColor);
+
+	return polygon;
+}
--- GlacoxEngineSFML/ShapeDrawer.h
+++ GlacoxEngineSFML/ShapeDrawer.h
@@ -4,5 +4,12 @@
 
 class ShapeDrawer : Graphics
 {
-};
+public:
+	ShapeDrawer();
 
+	sf::RectangleShape CreateSquare(float size, sf::Color color = sf::Color::Transparent, float posx = 0, float posy = 0, float OutlineThickness = 0, sf::Color OutlineColor = sf::Color::Transparent);
+	sf::RectangleShape CreateRectangle(float sizex, float sizey, sf::Color color = sf::Color::Transparent, float posx = 0, float posy = 0, float OutlineThickness = 0, sf::Color OutlineColor = sf::Color::Transparent);
+	sf::CircleShape CreateCircle(float radius, sf::Color color = sf::Color::Transparent, float posx = 0, float posy = 0, float OutlineThickness = 0, sf::Color OutlineColor = sf::Color::Transparent);
+	sf::CircleShape CreateTriangle(float radius, sf::Color color = sf::Color::Transparent, float posx = 0, float posy = 0, float OutlineThickness = 0, sf::Color OutlineColor = sf::Color::Transparent);
+	sf::CircleShape CreatePolygon(float radius, int sideNumber, sf::Color color = sf::Color::Transparent, float posx = 0, float posy = 0, float OutlineThickness = 0, sf::Color OutlineColor = sf::Color::Transparent);
+};
